33900f4eb9866217fc6fcb4596613c1321920189
Brendan <brendan.james.lim@gmail.com>
2026-01-10 13:51:30 -0800

    changed logo

--- hinto/src/main.ts
+++ hinto/src/main.ts
@@ -6,21 +6,23 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <ul>
         <li>Find the secret 5 letter word, you have unlimited guesses.</li>
         <li>Each guess will give you a hints relating to the secret word.</li>
-        <li>The dog will also change its expression based on how hot or cold you are to the word/li>
+        <li>The dog will also change its expression based on how hot or cold you are to the word</li>
       </ul>
     </aside>  
 <div class="content">
+  <div class="header">
     <a>
-      <img src="src/images/dog-check.png" class="logo vanilla" alt="Habinu Logo" />
+      <img src="src/images/dog-huh.png" class="logo vanilla" alt="Habinu Logo" />
     </a>
     <h1>Hinto</h1>
+  </div>
     <div class="textbox">
       <input type="text" id="simple-input" maxlength="5" placeholder="Enter 5-letter word"/>
     </div>
       <div class="card">
       <button id="counter" type="button">Enter</button>
     </div>
-    <p class=gemini >Gemini text here</p>
+    <p class="gemini">Gemini text here</p>
 
   </div>
 `
