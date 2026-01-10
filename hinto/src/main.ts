4653301fd6d4312c7e72c4c6ac70a71be9a77453
Alex Tran <alextran@Alexs-MacBook-Air-2.local>
2026-01-10 14:47:36 -0800

    removed obsolete imports

--- hinto/src/main.ts
+++ hinto/src/main.ts
@@ -1,6 +1,4 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
 import { getHint } from './hints.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
